2779524b9f407ad4aae99b2dd98ff4759b878701
Izhar Firdaus <kagesenshi.87@gmail.com>
2021-12-16 10:23:32 +0800

    added cli parameters for hive metadata exporter

--- hive_metadata_exporter.py
+++ hive_metadata_exporter.py
@@ -7,25 +7,33 @@ import json
 import sqlite3
 import time
 import logging
+from argparse import ArgumentParser
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('hive-metadata-exporter')
 
-jar_files=glob.glob('/usr/hdp/current/hive-client/jdbc/*.jar')
+parser = ArgumentParser()
+parser.add_argument('-H', '--host', required=True, help='Hive JDBC Host')
+parser.add_argument('-p', '--port', default=10000, type=int,
+        help='Hive JDBC Port')
+parser.add_argument('-j', '--jars', help='JAR files to include', default='')
+parser.add_argument('-d', '--driver', help='Hive JDBC driver class',
+        default='org.apache.hive.jdbc.HiveDriver')
+parser.add_argument('-e', '--exportfile', default='hive-metadata-export.sav', help='Export file')
+args = parser.parse_args()
 
-host='10.10.10.31'
-port='10000'
-database='default'
-driver='org.apache.hive.jdbc.HiveDriver'
+jar_files = []
+for jp in args.jars.split(','):
+    jp = jp.strip()
+    if not jp:
+        continue
+    jar_files += glob.glob(jp)
 
-
-conn_hive = jaydebeapi.connect(driver,
-    'jdbc:hive2://10.10.10.31:10000/default;',jars=jar_files)
-
+conn_hive = jaydebeapi.connect(args.driver,
+    'jdbc:hive2://%s:%s/default;' % (args.host, args.port),jars=jar_files)
 
 def get_createstmt(cur, db, tbl):
     cur.execute('show create table %s.%s' % (db,tbl))
-    
     return '\n'.join([c[0] for c in cur.fetchall()])
 
 cur = conn_hive.cursor()
@@ -35,7 +43,7 @@ databases = [c[0] for c in cur.fetchall()]
 
 all_tables = []
 
-outdb = sqlite3.connect('output.sqlite')
+outdb = sqlite3.connect(args.exportfile)
 
 outdb.execute('''
     create table if not exists exported_tables (
